611b1ff0e40718d2d906db9d0d73f8c7cced41ba
Amelia Enakirerhi <enaki.aa@outlook.com>
2022-05-30 15:33:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,24 @@
 # find_anagrams("hello", "check") --> False
 # find_anagrams("below", "elbow") --> True
 
-
 def find_anagram(word, anagram):
-    # [assignment] Add your code here
+     # [assignment] Add your code here
 
     return True
+
+str1 = "Tars"
+str2 = "Rats"
+
+if(len(str1) == len(str2))
+
+sorted_str1 = sorted(str1)
+sorted_str2 = sorted(str2)
+
+if(sorted_str1 == sorted_str2):
+    print(str1 + " and " + str2 + are anagrams. ")
+else:
+    print(str1 + " and " + str2 + are not anagrams. ")
+          
+
+
 
